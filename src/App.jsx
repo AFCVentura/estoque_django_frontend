e51147baf92beb6a0b251130d60e8f532c11b97b
Ventura <joao.ventura1@alunos.sc.senac.br>
2024-11-14 21:51:17 -0300

    feat: navbar base code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import NavbarComponent from "./components/NavbarComponent";
+
+function App() {
+  return (
+    <>
+      <NavbarComponent />
+    </>
+  );
+}
+
+export default App;
